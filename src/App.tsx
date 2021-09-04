583186c9d79228797874a7998cb9428da4b434a7
Mylena Munhoz <mylena.prado@edu.unipar.br>
2021-09-03 21:53:23 -0300

    add estilizacao

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,34 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
-import { GlobalStyle } from "./styles/global";
+import Modal from 'react-modal';
 
+import { GlobalStyle } from "./styles/global";
+import { NewTransactionModal } from "./components/NewTransactionModal";
+
+Modal.setAppElement('#root')
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true)
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false)
+  }
+
   return (
     <Fragment>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+
+      <NewTransactionModal 
+      isOpen={isNewTransactionModalOpen}
+      onRequestClose={handleCloseNewTransactionModal}
+      />
+
       <GlobalStyle />
     </Fragment>
   );
